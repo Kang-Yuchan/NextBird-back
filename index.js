d330c8a36f91bfef773343a8ace89a46f793f20a
Yuchan <mikuplayer123@gmail.com>
2020-06-20 19:37:44 +0900

    Backend API

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const db = require("./models");
+const express = require('express');
+const dotenv = require('dotenv');
+const db = require('./models');
+const userAPIRouter = require('./routes/user');
+const postAPIRouter = require('./routes/post');
+const postsAPIRouter = require('./routes/posts');
+
 dotenv.config();
 const app = express();
 db.sequelize.sync();
 
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello, express server!");
-});
-
-app.get("/about", (req, res) => {
-  res.send("Hello, about!");
-});
+app.use('/api/user', userAPIRouter);
+app.use('/api/post', postAPIRouter);
+app.use('/api/posts', postsAPIRouter);
 
 app.listen(PORT, () => {
-  console.log(
-    PORT
-      ? `Listening server: localhost:${PORT} 👌`
-      : "Your server is dead... 💀"
-  );
+	console.log(PORT ? `Listening server: localhost:${PORT} 👌` : 'Your server is dead... 💀');
 });
