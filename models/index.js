968d68f36f3ee912d133817e90ed01a8b15ae548
Kang.Yuchan <ikimaku0517@gmail.com>
2020-06-20 17:21:36 +0900

    Connect & Create Models

--- models/index.js
+++ models/index.js
@@ -1,21 +1,21 @@
-const Sequelize = require("sequelize");
-const env = process.env.NODE_ENV || "development";
-const config = require("../config/config.json")[env];
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config.json')[env];
 const db = {};
 
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+	if (db[modelName].associate) {
+		db[modelName].associate(db);
+	}
 });
 
+db.User = require('./user.js')(sequelize, Sequelize);
+db.Post = require('./post.js')(sequelize, Sequelize);
+db.Hashtag = require('./hashtag.js')(sequelize, Sequelize);
+db.Image = require('./image.js')(sequelize, Sequelize);
+db.Comment = require('./comment.js')(sequelize, Sequelize);
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
