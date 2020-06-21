e601b77d0a0f67d157a796e5cf68568bdecf9dca
Yuchan <mikuplayer123@gmail.com>
2020-06-21 22:28:00 +0900

    load posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const db = require('../models');
 const router = express.Router();
 
-router.get('/', (req, res) => {});
+router.get('/', async (req, res, next) => {
+	// GET /api/posts
+	try {
+		const posts = await db.Post.findAll({
+			include: [
+				{
+					model: db.User,
+					attributes: [ 'id', 'userId' ]
+				}
+			],
+			order: [ [ 'createdAt', 'DESC' ] ]
+		});
+		res.json(posts);
+	} catch (error) {
+		console.error(error);
+		next(error);
+	}
+});
 
 module.exports = router;
